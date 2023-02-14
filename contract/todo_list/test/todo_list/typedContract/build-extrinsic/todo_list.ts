85c97c952d3da811b1a6ab36c685208b662afe66
Ganesh oli <ganesh@thaiofficefurniture.com>
2023-02-14 13:10:24 +0545

    first commit

---
+++ contract/todo_list/test/todo_list/typedContract/build-extrinsic/todo_list.ts
@@ -0,0 +1,77 @@
+/* This file is auto-generated */
+
+import type { ContractPromise } from '@polkadot/api-contract';
+import type { GasLimit, GasLimitAndRequiredValue } from '@supercolony/typechain-types';
+import { buildSubmittableExtrinsic } from '@supercolony/typechain-types';
+import type * as ArgumentTypes from '../types-arguments/todo_list';
+import type BN from 'bn.js';
+
+
+
+export default class Methods {
+	private __nativeContract : ContractPromise;
+
+	constructor(
+		nativeContract : ContractPromise,
+	) {
+		this.__nativeContract = nativeContract;
+	}
+	/**
+	 * createTodoItem
+	 *
+	 * @param { string } itemName,
+	 * @param { ArgumentTypes.Prioritise } priority,
+	*/
+	"createTodoItem" (
+		itemName: string,
+		priority: ArgumentTypes.Prioritise,
+		__options: GasLimit,
+	){
+		return buildSubmittableExtrinsic( this.__nativeContract, "createTodoItem", [itemName, priority], __options);
+	}
+
+	/**
+	 * updateTodoItem
+	 *
+	 * @param { (number | string | BN) } itemId,
+	*/
+	"updateTodoItem" (
+		itemId: (number | string | BN),
+		__options: GasLimit,
+	){
+		return buildSubmittableExtrinsic( this.__nativeContract, "updateTodoItem", [itemId], __options);
+	}
+
+	/**
+	 * getTodoList
+	 *
+	*/
+	"getTodoList" (
+		__options: GasLimit,
+	){
+		return buildSubmittableExtrinsic( this.__nativeContract, "getTodoList", [], __options);
+	}
+
+	/**
+	 * removeTodoItem
+	 *
+	 * @param { (number | string | BN) } itemId,
+	*/
+	"removeTodoItem" (
+		itemId: (number | string | BN),
+		__options: GasLimit,
+	){
+		return buildSubmittableExtrinsic( this.__nativeContract, "removeTodoItem", [itemId], __options);
+	}
+
+	/**
+	 * getTodoLength
+	 *
+	*/
+	"getTodoLength" (
+		__options: GasLimit,
+	){
+		return buildSubmittableExtrinsic( this.__nativeContract, "getTodoLength", [], __options);
+	}
+
+}
---
+++ ui/components/abi.ts
@@ -0,0 +1,501 @@
+const abi = {
+  "source": {
+    "hash": "0x488294784c6ea1a835235ba6bed3d2ce5711bd9f7453f7fabf6e0e8499988ab8",
+    "language": "ink! 3.4.0",
+    "compiler": "rustc 1.69.0-nightly"
+  },
+  "contract": {
+    "name": "todo_list",
+    "version": "0.1.0",
+    "authors": [
+      "Ganesh oli"
+    ]
+  },
+  "V3": {
+    "spec": {
+      "constructors": [
+        {
+          "args": [
+            {
+              "label": "init_value",
+              "type": {
+                "displayName": [
+                  "bool"
+                ],
+                "type": 7
+              }
+            }
+          ],
+          "docs": [],
+          "label": "new",
+          "payable": false,
+          "selector": "0x9bae9d5e"
+        }
+      ],
+      "docs": [],
+      "events": [
+        {
+          "args": [
+            {
+              "docs": [],
+              "indexed": true,
+              "label": "item",
+              "type": {
+                "displayName": [
+                  "TodoItem"
+                ],
+                "type": 5
+              }
+            }
+          ],
+          "docs": [],
+          "label": "ItemCreated"
+        },
+        {
+          "args": [
+            {
+              "docs": [],
+              "indexed": true,
+              "label": "item",
+              "type": {
+                "displayName": [
+                  "TodoItem"
+                ],
+                "type": 5
+              }
+            }
+          ],
+          "docs": [],
+          "label": "ItemUpdated"
+        },
+        {
+          "args": [
+            {
+              "docs": [],
+              "indexed": true,
+              "label": "item_id",
+              "type": {
+                "displayName": [
+                  "TodoItemId"
+                ],
+                "type": 4
+              }
+            }
+          ],
+          "docs": [],
+          "label": "ItemRemoved"
+        }
+      ],
+      "messages": [
+        {
+          "args": [
+            {
+              "label": "item_name",
+              "type": {
+                "displayName": [
+                  "String"
+                ],
+                "type": 6
+              }
+            },
+            {
+              "label": "priority",
+              "type": {
+                "displayName": [
+                  "Prioritise"
+                ],
+                "type": 8
+              }
+            }
+          ],
+          "docs": [],
+          "label": "create_todo_item",
+          "mutates": true,
+          "payable": false,
+          "returnType": {
+            "displayName": [
+              "Result"
+            ],
+            "type": 10
+          },
+          "selector": "0xbc82dad0"
+        },
+        {
+          "args": [
+            {
+              "label": "item_id",
+              "type": {
+                "displayName": [
+                  "TodoItemId"
+                ],
+                "type": 4
+              }
+            }
+          ],
+          "docs": [],
+          "label": "update_todo_item",
+          "mutates": true,
+          "payable": false,
+          "returnType": {
+            "displayName": [
+              "Result"
+            ],
+            "type": 10
+          },
+          "selector": "0xaf253ecf"
+        },
+        {
+          "args": [],
+          "docs": [],
+          "label": "get_todo_list",
+          "mutates": false,
+          "payable": false,
+          "returnType": {
+            "displayName": [
+              "Vec"
+            ],
+            "type": 13
+          },
+          "selector": "0xda72eabd"
+        },
+        {
+          "args": [
+            {
+              "label": "item_id",
+              "type": {
+                "displayName": [
+                  "TodoItemId"
+                ],
+                "type": 4
+              }
+            }
+          ],
+          "docs": [],
+          "label": "remove_todo_item",
+          "mutates": true,
+          "payable": false,
+          "returnType": {
+            "displayName": [
+              "Result"
+            ],
+            "type": 10
+          },
+          "selector": "0x71ab0786"
+        },
+        {
+          "args": [],
+          "docs": [],
+          "label": "get_todo_length",
+          "mutates": false,
+          "payable": false,
+          "returnType": {
+            "displayName": [
+              "i32"
+            ],
+            "type": 4
+          },
+          "selector": "0x43d225ee"
+        }
+      ]
+    },
+    "storage": {
+      "struct": {
+        "fields": [
+          {
+            "layout": {
+              "cell": {
+                "key": "0x0000000000000000000000000000000000000000000000000000000000000000",
+                "ty": 0
+              }
+            },
+            "name": "owner"
+          },
+          {
+            "layout": {
+              "cell": {
+                "key": "0x0100000000000000000000000000000000000000000000000000000000000000",
+                "ty": 3
+              }
+            },
+            "name": "item"
+          },
+          {
+            "layout": {
+              "cell": {
+                "key": "0x0200000000000000000000000000000000000000000000000000000000000000",
+                "ty": 4
+              }
+            },
+            "name": "item_id"
+          }
+        ]
+      }
+    },
+    "types": [
+      {
+        "id": 0,
+        "type": {
+          "def": {
+            "composite": {
+              "fields": [
+                {
+                  "type": 1,
+                  "typeName": "[u8; 32]"
+                }
+              ]
+            }
+          },
+          "path": [
+            "ink_env",
+            "types",
+            "AccountId"
+          ]
+        }
+      },
+      {
+        "id": 1,
+        "type": {
+          "def": {
+            "array": {
+              "len": 32,
+              "type": 2
+            }
+          }
+        }
+      },
+      {
+        "id": 2,
+        "type": {
+          "def": {
+            "primitive": "u8"
+          }
+        }
+      },
+      {
+        "id": 3,
+        "type": {
+          "def": {
+            "composite": {
+              "fields": [
+                {
+                  "name": "offset_key",
+                  "type": 9,
+                  "typeName": "Key"
+                }
+              ]
+            }
+          },
+          "params": [
+            {
+              "name": "K",
+              "type": 4
+            },
+            {
+              "name": "V",
+              "type": 5
+            }
+          ],
+          "path": [
+            "ink_storage",
+            "lazy",
+            "mapping",
+            "Mapping"
+          ]
+        }
+      },
+      {
+        "id": 4,
+        "type": {
+          "def": {
+            "primitive": "i32"
+          }
+        }
+      },
+      {
+        "id": 5,
+        "type": {
+          "def": {
+            "composite": {
+              "fields": [
+                {
+                  "name": "owner",
+                  "type": 0,
+                  "typeName": "AccountId"
+                },
+                {
+                  "name": "item_name",
+                  "type": 6,
+                  "typeName": "String"
+                },
+                {
+                  "name": "is_completed",
+                  "type": 7,
+                  "typeName": "bool"
+                },
+                {
+                  "name": "priority",
+                  "type": 8,
+                  "typeName": "Prioritise"
+                }
+              ]
+            }
+          },
+          "path": [
+            "todo_list",
+            "todo_list",
+            "TodoItem"
+          ]
+        }
+      },
+      {
+        "id": 6,
+        "type": {
+          "def": {
+            "primitive": "str"
+          }
+        }
+      },
+      {
+        "id": 7,
+        "type": {
+          "def": {
+            "primitive": "bool"
+          }
+        }
+      },
+      {
+        "id": 8,
+        "type": {
+          "def": {
+            "variant": {
+              "variants": [
+                {
+                  "index": 0,
+                  "name": "HIGH"
+                },
+                {
+                  "index": 1,
+                  "name": "LOW"
+                },
+                {
+                  "index": 2,
+                  "name": "MEDIUM"
+                }
+              ]
+            }
+          },
+          "path": [
+            "todo_list",
+            "todo_list",
+            "Prioritise"
+          ]
+        }
+      },
+      {
+        "id": 9,
+        "type": {
+          "def": {
+            "composite": {
+              "fields": [
+                {
+                  "type": 1,
+                  "typeName": "[u8; 32]"
+                }
+              ]
+            }
+          },
+          "path": [
+            "ink_primitives",
+            "Key"
+          ]
+        }
+      },
+      {
+        "id": 10,
+        "type": {
+          "def": {
+            "variant": {
+              "variants": [
+                {
+                  "fields": [
+                    {
+                      "type": 11
+                    }
+                  ],
+                  "index": 0,
+                  "name": "Ok"
+                },
+                {
+                  "fields": [
+                    {
+                      "type": 12
+                    }
+                  ],
+                  "index": 1,
+                  "name": "Err"
+                }
+              ]
+            }
+          },
+          "params": [
+            {
+              "name": "T",
+              "type": 11
+            },
+            {
+              "name": "E",
+              "type": 12
+            }
+          ],
+          "path": [
+            "Result"
+          ]
+        }
+      },
+      {
+        "id": 11,
+        "type": {
+          "def": {
+            "tuple": []
+          }
+        }
+      },
+      {
+        "id": 12,
+        "type": {
+          "def": {
+            "variant": {
+              "variants": [
+                {
+                  "index": 0,
+                  "name": "ItemNotExists"
+                },
+                {
+                  "index": 1,
+                  "name": "NotAOwner"
+                }
+              ]
+            }
+          },
+          "path": [
+            "todo_list",
+            "todo_list",
+            "TodoError"
+          ]
+        }
+      },
+      {
+        "id": 13,
+        "type": {
+          "def": {
+            "sequence": {
+              "type": 5
+            }
+          }
+        }
+      }
+    ]
+  }
+};
+
+export default abi;
