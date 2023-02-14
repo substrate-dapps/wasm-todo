69afc73f15ad394f1bd893cd37bd197dee2d2a61
vadim.nicolai-EXT <vadim.nicolai-EXT@vitrifi.net>
2023-02-14 17:36:17 +0200

    Small improvement.

--- ui/components/app.tsx
+++ ui/components/app.tsx
@@ -1,146 +1,157 @@
-import { options } from '@astar-network/astar-api';
-import { ApiPromise, WsProvider } from '@polkadot/api';
-import { Abi, ContractPromise } from '@polkadot/api-contract';
+import { Button, Input, Space, Divider, List } from "antd";
+import { options } from "@astar-network/astar-api";
+import { ApiPromise, WsProvider } from "@polkadot/api";
+import { Abi, ContractPromise } from "@polkadot/api-contract";
 import {
   isWeb3Injected,
-  web3Accounts, web3Enable
-} from '@polkadot/extension-dapp';
-import type { InjectedAccountWithMeta, InjectedExtension } from '@polkadot/extension-inject/types';
-import type { WeightV2 } from '@polkadot/types/interfaces';
-import type { NextPage } from 'next';
-import { useCallback, useEffect, useState } from 'react';
+  web3Accounts,
+  web3Enable,
+} from "@polkadot/extension-dapp";
+import type {
+  InjectedAccountWithMeta,
+  InjectedExtension,
+} from "@polkadot/extension-inject/types";
+import type { WeightV2 } from "@polkadot/types/interfaces";
+import type { NextPage } from "next";
+import { useCallback, useEffect, useState } from "react";
 
-import Head from 'next/head';
-import styles from '../styles/Home.module.css';
-import abiData from './abi';
-import Spinner from './spinner';
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import abiData from "./abi";
+import AddItem from "./AddItem";
 
 // local
 // const WS_PROVIDER = 'ws://127.0.0.1:9944'
 
 // shibuya
-const WS_PROVIDER = 'wss://shibuya-rpc.dwellir.com'
+const WS_PROVIDER = "wss://shibuya-rpc.dwellir.com";
 
-const proofSize = 131072
-const refTime = 6219235328
-const storageDepositLimit = null
+const proofSize = 131072;
+const refTime = 6219235328;
+const storageDepositLimit = null;
 
 const Home: NextPage = () => {
-  const [address, setAddress] = useState('')
-  const [addressSubmitted, setAddressSubmitted] = useState(false)
-  const [value, setValue] = useState('')
-  const [loading, setLoading] = useState(false)
-  const [account, setAccount] = useState<string>('')
-  const [accounts, setAccounts] = useState<InjectedAccountWithMeta[]>([])
-  const [extensions, setExtensions] = useState<InjectedExtension[]>([])
+  const [address, setAddress] = useState("");
+  const [addressSubmitted, setAddressSubmitted] = useState(false);
+  const [value, setValue] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [account, setAccount] = useState<string>("");
+  const [accounts, setAccounts] = useState<InjectedAccountWithMeta[]>([]);
+  const [extensions, setExtensions] = useState<InjectedExtension[]>([]);
+
+  const [newTodo, setTodo] = useState("");
 
   useEffect(() => {
     if (address) {
-      read().catch(console.error)
+      read().catch(console.error);
     }
-  }, [address])
+  }, [address]);
 
   // load Substrate wallet and set the signer
   const initSubstrateProvider = useCallback(async () => {
     if (!isWeb3Injected) {
-      throw new Error('The user does not have any Substrate wallet installed')
+      throw new Error("The user does not have any Substrate wallet installed");
     }
 
-    const extensions = await web3Enable('Flipper UI')
+    const extensions = await web3Enable("Flipper UI");
 
     if (extensions.length > 0) {
-      setExtensions(extensions)
+      setExtensions(extensions);
     }
 
     // set the first wallet as the signer (we assume there is only one wallet)
     // wallet.substrate.setSigner(extensions[0].signer)
 
-    const injectedAccounts = await web3Accounts()
+    const injectedAccounts = await web3Accounts();
 
     if (injectedAccounts.length > 0) {
-      setAccounts(injectedAccounts)
+      setAccounts(injectedAccounts);
     }
-  }, [])
+  }, []);
 
   const handleOnSelect = async (event: any) => {
-    setAccount(event.target.value)
-  }
+    setAccount(event.target.value);
+  };
 
-  const query = async (api: ApiPromise, contract: ContractPromise, address: string) => {
+  const query = async (
+    api: ApiPromise,
+    contract: ContractPromise,
+    address: string
+  ) => {
     // (We perform the send from an account, here using Alice's address)
     const { gasRequired, result, output } = await contract.query.getTodoList(
       address,
       {
-        gasLimit: api.registry.createType('WeightV2', {
+        gasLimit: api.registry.createType("WeightV2", {
           refTime,
           proofSize,
         }) as WeightV2,
         storageDepositLimit,
       }
-    )
+    );
 
     // const result = await api.call.contractsApi.call(address, contract.address, 0, null, null, msg.toU8a(msg.args.map((_) => account.address)))
 
     // The actual result from RPC as `ContractExecResult`
-    console.log(result.toHuman())
+    console.log(result.toHuman());
 
     // the gas consumed for contract execution
-    console.log(gasRequired.toHuman())
+    console.log(gasRequired.toHuman());
 
     // check if the call was successful
     if (result.isOk) {
       // output the return value
-      console.log('Success', output?.toHuman())
+      console.log("Success", output?.toHuman());
 
       if (output) {
-        setValue(output?.toString())
+        setValue(output?.toString());
       }
     } else {
-      console.error('Error', result.asErr)
+      console.error("Error", result.asErr);
     }
-  }
+  };
 
   const read = async () => {
-    const provider = new WsProvider(WS_PROVIDER)
-		const api = new ApiPromise(options({ provider }))
+    const provider = new WsProvider(WS_PROVIDER);
+    const api = new ApiPromise(options({ provider }));
 
-    await api.isReady
+    await api.isReady;
 
-    const abi = new Abi(abiData, api.registry.getChainProperties())
+    const abi = new Abi(abiData, api.registry.getChainProperties());
 
-    const contract = new ContractPromise(api, abi, address)
+    const contract = new ContractPromise(api, abi, address);
 
-    await query(api, contract, address)
-  }
+    await query(api, contract, address);
+  };
 
   const flip = async () => {
-    const provider = new WsProvider(WS_PROVIDER)
-		const api = new ApiPromise(options({ provider }))
+    const provider = new WsProvider(WS_PROVIDER);
+    const api = new ApiPromise(options({ provider }));
 
-    await api.isReady
+    await api.isReady;
 
-    api.setSigner(extensions[0].signer)
+    api.setSigner(extensions[0].signer);
 
-    console.log('API is ready')
+    console.log("API is ready");
 
-    const abi = new Abi(abiData, api.registry.getChainProperties())
+    const abi = new Abi(abiData, api.registry.getChainProperties());
 
-    const contract = new ContractPromise(api, abi, address)
+    const contract = new ContractPromise(api, abi, address);
 
-    const { gasRequired, result, output } = await contract.query.flip(
-      address,
-      {
-        gasLimit: api.registry.createType('WeightV2', {
-          refTime,
-          proofSize,
-        }) as WeightV2,
-        storageDepositLimit,
-      }
-    )
+    const { gasRequired, result, output } = await contract.query.flip(address, {
+      gasLimit: api.registry.createType("WeightV2", {
+        refTime,
+        proofSize,
+      }) as WeightV2,
+      storageDepositLimit,
+    });
 
-    const gasLimit = api.registry.createType('WeightV2', gasRequired) as WeightV2
+    const gasLimit = api.registry.createType(
+      "WeightV2",
+      gasRequired
+    ) as WeightV2;
 
-    setLoading(true)
+    setLoading(true);
 
     // Send the transaction, like elsewhere this is a normal extrinsic
     // with the same rules as applied in the API (As with the read example,
@@ -149,72 +160,122 @@ const Home: NextPage = () => {
       await contract.tx
         .flip({
           gasLimit: gasLimit,
-          storageDepositLimit
+          storageDepositLimit,
         })
         .signAndSend(account, async (res) => {
           if (res.status.isInBlock) {
-            console.log('in a block')
-            setLoading(false)
+            console.log("in a block");
+            setLoading(false);
           } else if (res.status.isFinalized) {
-            console.log('finalized')
+            console.log("finalized");
           }
-        })
+        });
 
-      await query(api, contract, address)
+      await query(api, contract, address);
     } catch (e) {
-      console.error(e)
-      setLoading(false)
+      console.error(e);
+      setLoading(false);
     }
-  }
+  };
 
+  const addTodoItem = async () => {
+    const provider = new WsProvider(WS_PROVIDER);
+    const api = new ApiPromise(options({ provider }));
+
+    await api.isReady;
+
+    api.setSigner(extensions[0].signer);
+
+    const abi = new Abi(abiData, api.registry.getChainProperties());
+
+    const contract = new ContractPromise(api, abi, address);
+    const gasLimit = api.registry.createType("WeightV2", {
+      refTime,
+      proofSize,
+    }) as any;
+
+    setLoading(true);
+
+    // Send the transaction, like elsewhere this is a normal extrinsic
+    // with the same rules as applied in the API (As with the read example,
+    // additional params, if required can follow)
+    try {
+      await contract.tx
+        .createTodoItem(
+          {
+            gasLimit: gasLimit,
+            storageDepositLimit,
+          },
+          newTodo,
+          "low"
+        )
+        .signAndSend(account, async (res) => {
+          if (res.status.isInBlock) {
+            console.log("in a block");
+            setLoading(false);
+          } else if (res.status.isFinalized) {
+            console.log("finalized");
+          }
+        });
+    } catch (e) {
+      console.error(e);
+      setLoading(false);
+    }
+  };
 
   return (
     <div className={styles.container}>
       <Head>
         <title>Flipper Contract</title>
-        <meta name='description' content='Flipper Contract' />
-        <link rel='icon' href='/favicon.ico' />
+        <meta name="description" content="Flipper Contract" />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      {loading ? <Spinner /> : null }
-
       <main className={styles.main}>
-        {addressSubmitted ? <>
-          <h3 className={styles.title}>
-            Flipper Contract
-          </h3>
+        {addressSubmitted ? (
+          <>
+            <h3 className={styles.title}>Flipper Contract</h3>
 
-          <button onClick={initSubstrateProvider}>Load Wallets</button><br />
+            <button onClick={initSubstrateProvider}>Load Wallets</button>
+            <br />
 
-          <select onChange={handleOnSelect}>
-            <option value="">Select Address</option>
-            {accounts.map(account => (
-              <option key={account.address} value={account.address}>{account.meta.name} {account.address}</option>
-            ))}
-          </select><br />
+            <select onChange={handleOnSelect}>
+              <option value="">Select Address</option>
+              {accounts.map((account) => (
+                <option key={account.address} value={account.address}>
+                  {account.meta.name} {account.address}
+                </option>
+              ))}
+            </select>
+            <br />
 
-          <div>
-            <button onClick={read}>Read</button>
-            <button onClick={flip}>Flip</button>
-          </div>
-          <h4>{value}</h4>
-        </> :
-        <>
-          <h3 className={styles.title}>
-            Provide Contract Address
-          </h3>
-          <div className={styles.address}>
-            <input
-              type='text'
-              value={address}
-              onChange={e => setAddress(e.target.value)}
-            />
-            <button onClick={e => setAddressSubmitted(true)}>Set</button>
-          </div>
-        </>}
+            <div>
+              <button onClick={read}>Read</button>
+              <button onClick={flip}>Flip</button>
+            </div>
+            <h4>{value}</h4>
+          </>
+        ) : (
+          <>
+            <h3 className={styles.title}>Provide Contract Address</h3>
+            <div className={styles.address}>
+              <input
+                type="text"
+                value={address}
+                onChange={(e) => setAddress(e.target.value)}
+              />
+              <button onClick={(e) => setAddressSubmitted(true)}>Set</button>
+            </div>
+          </>
+        )}
+        <AddItem
+          account={account}
+          address={address}
+          signer={extensions?.[0]?.signer}
+        />
       </main>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
--- ui/components/spinner.tsx
+++
@@ -1,5 +0,0 @@
-const Spinner = () => <div className="loaderContainer">
-  <div className="loader"></div>
-</div>;
-
-export default Spinner;
